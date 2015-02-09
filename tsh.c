972081ee2dbccbf7ddf0be1f26ca4c5a9c3d5505
rgoo001 <ryankgoo@gmail.com>
2015-02-08 17:45:34 -0800

    cmdline->argv

--- tsh.c
+++ tsh.c
@@ -253,7 +253,7 @@ int parseline(const char *cmdline, char **argv)
 int builtin_cmd(char **argv) 
 {
     printf("cmdline[0]: %s\n", argv[0]);
-    if (parseline(cmdline,cmdline)) printf("hello?\n");
+   // if (parseline(cmdline,cmdline)) printf("hello?\n");
     return 0;     /* not a builtin command */
 }
 
